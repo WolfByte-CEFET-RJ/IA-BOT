ddd7a4f6892ce3e3f1047d12e66053edd254dc94
Thiago <thiagoandre2121@gmail.com>
2021-06-18 21:17:36 -0300

    implementando a classe Layer_dense

--- Scripts/main.py
+++ Scripts/main.py
@@ -18,6 +18,15 @@ def sigmoid(x):
 
 def relu(x):
     return np.maximun(0, x)
+
+#classe para criar uma camada
+class Layer_Dense():
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
         
 
 #importando dataset
