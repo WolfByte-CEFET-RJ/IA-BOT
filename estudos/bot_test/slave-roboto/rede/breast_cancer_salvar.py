8c9c6c8f0e25573d96a18f5a7e250195a98b27d5
dd2t <matheusddsm@gmail.com>
2021-07-02 16:50:34 -0300

    echo_bot

---
+++ estudos/bot_test/slave-roboto/rede/breast_cancer_salvar.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+previsores = pd.read_csv('entradas_breast.csv')
+classe = pd.read_csv('saidas_breast.csv')
+
+classificador = Sequential()
+classificador.add(Dense(units = 8, activation = 'relu', 
+                        kernel_initializer = 'normal', input_dim = 30))
+classificador.add(Dropout(0.2))
+classificador.add(Dense(units = 8, activation = 'relu', 
+                        kernel_initializer = 'normal'))
+classificador.add(Dropout(0.2))
+classificador.add(Dense(units = 1, activation = 'sigmoid'))
+classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                      metrics = ['binary_accuracy'])
+classificador.fit(previsores, classe, batch_size = 10, epochs = 100)
+
+classificador_json = classificador.to_json()
+with open('classificador_breast.json', 'w') as json_file:
+    json_file.write(classificador_json)
+classificador.save_weights('classificador_breast.h5')
+
---
+++ estudos/bot_test/slave-roboto/rede/breast_classificar_um_registro.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+
+previsores = pd.read_csv('entradas_breast.csv')
+classe = pd.read_csv('saidas_breast.csv')
+
+classificador = Sequential()
+classificador.add(Dense(units = 8, activation = 'relu', 
+                        kernel_initializer = 'normal', input_dim = 30))
+classificador.add(Dropout(0.2))
+classificador.add(Dense(units = 8, activation = 'relu', 
+                        kernel_initializer = 'normal'))
+classificador.add(Dropout(0.2))
+classificador.add(Dense(units = 1, activation = 'sigmoid'))
+classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy',
+                      metrics = ['binary_accuracy'])
+classificador.fit(previsores, classe, batch_size = 10, epochs = 100)
+
+novo = np.array([[15.80, 8.34, 118, 900, 0.10, 0.26, 0.08, 0.134, 0.178,
+                  0.20, 0.05, 1098, 0.87, 4500, 145.2, 0.005, 0.04, 0.05, 0.015,
+                  0.03, 0.007, 23.15, 16.64, 178.5, 2018, 0.14, 0.185,
+                  0.84, 158, 0.363]])
+previsao = classificador.predict(novo)
+previsao = (previsao > 0.5)
+
